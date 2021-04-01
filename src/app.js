eb938d6b8b6935243c89fb1a61e7b1988d4bfc72
taurus366 <47596673+taurus366@users.noreply.github.com>
2021-04-01 17:30:29 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import {render} from '../node_modules/lit-html/lit-html.js';
-import page from '../node_modules/page/page.mjs';
+import {render } from '//unpkg.com/lit-html?module';
+import page from '//unpkg.com/page/page.mjs';
 import {logout} from './api/data.js';
 
 const main = document.getElementById('site-content');
